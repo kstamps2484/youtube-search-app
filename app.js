d3bf6dc92923f2c00ea3182703271711de4cadc1
Kevin Stamps <kstamps2484@gmail.com>
2017-07-31 13:37:25 -0600

    implement app, and fetchApiData functions. start on displaySearchResults function

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
+//================= global variables =====================
 var YOUTUBE_SEARCH_URL = "";
 
 var RESULTS_TEMPLATE = (
   "<div>" +
   "</div>"
 );
+//================== functions ==========================
 
-function app {
+function fetchApiData(input, callback){
+  var query = {
+    q: input + ' in:name',
+    per_page: 5;
+  }
+  $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+}
+
+function displaySearchResults(data){
   
+}
+
+//=================== app: main function ===============================
+function app {
+  $('.youtube-search-form').submit( function(event){
+    event.preventDefault();
+    var inputElement = $('.youtube-search');
+    var query = searchElement.val();
+    inputElement.val('');
+    fetchApiData(query, displaySearchResults);
+  });
 }
 
 $(app);
